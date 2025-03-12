238d033e284a13e1f3738ab58d67e0184c1fc66a
cesarluilly <cesarluilly.garcia@hotmail.com>
2025-03-11 21:48:43 -0600

    12-tipos basicos y conceptos generales

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,36 @@
+
+
+const name = 'Cesar';
+let hpPoints: number | 'FULL' = 3;
+const isAlive: boolean = true;
+
+
+hpPoints = 'FULL';
+
+console.log(name, hpPoints, isAlive);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {};
+
+
+
+
+
+
+
+
+
