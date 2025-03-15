40a95eacf3ccb62c6bc572ffdfc5808f102a2b87
cesarluilly <cesarluilly.garcia@hotmail.com>
2025-03-14 21:06:26 -0600

    14 Funciones Basicas

---
+++ src/topics/03-functions.ts
@@ -0,0 +1,34 @@
+
+function addNumbers(a: number, b: number): number{
+    return a + b;
+}
+
+const addNumbersArrow = (a:number, b:number) : string=> {
+    return `${a + b}`;
+}
+
+const multiply = (firstNumber:number, 
+    secondNumber? :number, 
+    base:number = 2) : number=> {
+    return firstNumber * base
+}
+
+
+const result : number = addNumbers(1, 2);
+const result2 : string = addNumbersArrow(1, 2);
+
+console.log({result});
+console.log({result2});
+
+
+
+
+
+
+
+
+
+
+
+
+export {};
